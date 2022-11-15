e19d5e99c0e85072b9ea18c16e6b19c7de2ade2b
Prabhu Subramanian <prabhu@shiftleft.io>
2022-11-15 22:53:57 +0000

    Include osquery for linux

--- build.sh
+++ build.sh
@@ -13,6 +13,14 @@ sha256sum plugins/osquery/osqueryi-windows-amd64.exe > plugins/osquery/osqueryi-
 rm -rf osquery-5.6.0.windows_x86_64
 rm osquery-5.6.0.windows_x86_64.zip
 
+wget https://github.com/osquery/osquery/releases/download/5.6.0/osquery-5.6.0_1.linux_x86_64.tar.gz
+tar -xvf osquery-5.6.0_1.linux_x86_64.tar.gz
+cp opt/osquery/bin/osqueryd plugins/osquery/osqueryi-linux-amd64
+upx -1 plugins/osquery/osqueryi-linux-amd64
+sha256sum plugins/osquery/osqueryi-linux-amd64 > plugins/osquery/osqueryi-linux-amd64.sha256
+rm -rf etc usr var opt
+rm osquery-5.6.0_1.linux_x86_64.tar.gz
+
 for plug in goversion trivy cargo-auditable
 do
     mkdir -p plugins/$plug
@@ -24,6 +32,7 @@ do
     popd
 done
 
+./plugins/osquery/osqueryi-linux-amd64 --help
 ./plugins/goversion/goversion-linux-amd64
 ./plugins/trivy/trivy-cdxgen-linux-amd64 -v
 ./plugins/cargo-auditable/cargo-auditable-cdxgen-linux-amd64
