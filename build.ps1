5f8de2a70295a4f86b3c9c0036a3df9a17181f20
Prabhu Subramanian <prabhu@shiftleft.io>
2022-11-18 13:06:58 +0000

    Windows build script

---
+++ build.ps1
@@ -0,0 +1,39 @@
+Remove-Item plugins\goversion -Recurse -Force
+Remove-Item plugins\cargo-auditable -Recurse -Force
+Remove-Item plugins\osquery -Recurse -Force
+
+New-Item -ItemType Directory -Path plugins\goversion -Force
+New-Item -ItemType Directory -Path plugins\cargo-auditable -Force
+New-Item -ItemType Directory -Path plugins\osquery -Force
+
+Invoke-WebRequest -Uri https://github.com/upx/upx/releases/download/v4.0.0/upx-4.0.0-win64.zip -UseBasicParsing -OutFile upx-4.0.0-win64.zip
+Expand-Archive -Path upx-4.0.0-win64.zip -DestinationPath . -Force
+
+Invoke-WebRequest -Uri https://github.com/osquery/osquery/releases/download/5.6.0/osquery-5.6.0.windows_x86_64.zip -UseBasicParsing -OutFile osquery-5.6.0.windows_x86_64.zip
+Expand-Archive -Path osquery-5.6.0.windows_x86_64.zip -DestinationPath . -Force
+copy "osquery-5.6.0.windows_x86_64\Program Files\osquery\osqueryi.exe" plugins\osquery\osqueryi-windows-amd64.exe
+upx-4.0.0-win64\upx.exe -1 plugins\osquery\osqueryi-windows-amd64.exe
+
+set CGO_ENABLED=0
+set GOOS=windows
+set GOARCH=amd64
+New-Item -ItemType Directory -Path plugins\goversion -Force
+cd thirdparty\goversion
+go build -ldflags "-s -w" -o build\goversion-windows-amd64.exe
+..\..\upx-4.0.0-win64\upx.exe -1 build\goversion-windows-amd64.exe
+copy build\* ..\..\plugins\goversion\
+Remove-Item build -Recurse -Force
+cd ..\..
+
+New-Item -ItemType Directory -Path plugins\cargo-auditable -Force
+cd thirdparty\cargo-auditable
+go build -ldflags "-s -w" -o build\cargo-auditable-windows-amd64.exe
+..\..\upx-4.0.0-win64\upx.exe -1 build\cargo-auditable-windows-amd64.exe
+copy build\* ..\..\plugins\cargo-auditable\
+Remove-Item build -Recurse -Force
+cd ..\..
+
+Remove-Item osquery-5.6.0.windows_x86_64 -Recurse -Force
+Remove-Item osquery-5.6.0.windows_x86_64.zip -Recurse -Force
+Remove-Item upx-4.0.0-win64 -Recurse -Force
+Remove-Item upx-4.0.0-win64.zip -Recurse -Force
