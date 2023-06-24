955d3f61529f44c5fc3bc92067b1ee9a5f40d8da
Prabhu Subramanian <prabhu@appthreat.com>
2023-06-24 00:02:18 +0000

    Update packages
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- build.ps1
+++ build.ps1
@@ -8,7 +8,7 @@ Expand-Archive -Path upx-4.0.2-win64.zip -DestinationPath . -Force
 Invoke-WebRequest -Uri https://github.com/osquery/osquery/releases/download/5.8.2/osquery-5.8.2.windows_x86_64.zip -UseBasicParsing -OutFile osquery-5.8.2.windows_x86_64.zip
 Expand-Archive -Path osquery-5.8.2.windows_x86_64.zip -DestinationPath . -Force
 copy "osquery-5.8.2.windows_x86_64\Program Files\osquery\osqueryi.exe" plugins\osquery\osqueryi-windows-amd64.exe
-upx-4.0.2-win64\upx.exe -1 plugins\osquery\osqueryi-windows-amd64.exe
+upx-4.0.2-win64\upx.exe -9 --lzma plugins\osquery\osqueryi-windows-amd64.exe
 
 set CGO_ENABLED=0
 set GOOS=windows
@@ -16,7 +16,7 @@ set GOARCH=amd64
 New-Item -ItemType Directory -Path plugins\goversion -Force
 cd thirdparty\goversion
 go build -ldflags "-H=windowsgui -s -w" -o build\goversion-windows-amd64.exe
-..\..\upx-4.0.2-win64\upx.exe -1 build\goversion-windows-amd64.exe
+..\..\upx-4.0.2-win64\upx.exe -9 --lzma build\goversion-windows-amd64.exe
 copy build\* ..\..\plugins\goversion\
 Remove-Item build -Recurse -Force
 cd ..\..
@@ -24,7 +24,7 @@ cd ..\..
 New-Item -ItemType Directory -Path plugins\cargo-auditable -Force
 cd thirdparty\cargo-auditable
 go build -ldflags "-H=windowsgui -s -w" -o build\cargo-auditable-windows-amd64.exe
-..\..\upx-4.0.2-win64\upx.exe -1 build\cargo-auditable-windows-amd64.exe
+..\..\upx-4.0.2-win64\upx.exe -9 --lzma build\cargo-auditable-windows-amd64.exe
 copy build\* ..\..\plugins\cargo-auditable\
 Remove-Item build -Recurse -Force
 cd ..\..
