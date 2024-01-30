5d889f1f2583eaf7b7120a39d24216c7d442f159
Prabhu Subramanian <prabhu@appthreat.com>
2024-01-30 18:52:48 +0000

    darwin arm64 package
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- build.ps1
+++ build.ps1
@@ -3,13 +3,13 @@ New-Item -ItemType Directory -Path plugins\cargo-auditable -Force
 New-Item -ItemType Directory -Path plugins\osquery -Force
 New-Item -ItemType Directory -Path plugins\dosai -Force
 
-Invoke-WebRequest -Uri https://github.com/upx/upx/releases/download/v4.2.1/upx-4.2.1-win64.zip -UseBasicParsing -OutFile upx-4.2.1-win64.zip
-Expand-Archive -Path upx-4.2.1-win64.zip -DestinationPath . -Force
+Invoke-WebRequest -Uri https://github.com/upx/upx/releases/download/v4.2.2/upx-4.2.2-win64.zip -UseBasicParsing -OutFile upx-4.2.2-win64.zip
+Expand-Archive -Path upx-4.2.2-win64.zip -DestinationPath . -Force
 
-Invoke-WebRequest -Uri https://github.com/osquery/osquery/releases/download/5.10.2/osquery-5.10.2.windows_x86_64.zip -UseBasicParsing -OutFile osquery-5.10.2.windows_x86_64.zip
-Expand-Archive -Path osquery-5.10.2.windows_x86_64.zip -DestinationPath . -Force
-copy "osquery-5.10.2.windows_x86_64\Program Files\osquery\osqueryi.exe" plugins\osquery\osqueryi-windows-amd64.exe
-upx-4.2.1-win64\upx.exe -9 --lzma plugins\osquery\osqueryi-windows-amd64.exe
+Invoke-WebRequest -Uri https://github.com/osquery/osquery/releases/download/5.11.0/osquery-5.11.0.windows_x86_64.zip -UseBasicParsing -OutFile osquery-5.11.0.windows_x86_64.zip
+Expand-Archive -Path osquery-5.11.0.windows_x86_64.zip -DestinationPath . -Force
+copy "osquery-5.11.0.windows_x86_64\Program Files\osquery\osqueryi.exe" plugins\osquery\osqueryi-windows-amd64.exe
+upx-4.2.2-win64\upx.exe -9 --lzma plugins\osquery\osqueryi-windows-amd64.exe
 
 Invoke-WebRequest -Uri https://github.com/owasp-dep-scan/dosai/releases/latest/download/Dosai.exe -UseBasicParsing -OutFile plugins/dosai/dosai-windows-amd64.exe
 
@@ -19,7 +19,7 @@ set GOARCH=amd64
 New-Item -ItemType Directory -Path plugins\goversion -Force
 cd thirdparty\goversion
 go build -ldflags "-H=windowsgui -s -w" -o build\goversion-windows-amd64.exe
-..\..\upx-4.2.1-win64\upx.exe -9 --lzma build\goversion-windows-amd64.exe
+..\..\upx-4.2.2-win64\upx.exe -9 --lzma build\goversion-windows-amd64.exe
 copy build\* ..\..\plugins\goversion\
 Remove-Item build -Recurse -Force
 cd ..\..
@@ -27,7 +27,7 @@ cd ..\..
 New-Item -ItemType Directory -Path plugins\cargo-auditable -Force
 cd thirdparty\cargo-auditable
 go build -ldflags "-H=windowsgui -s -w" -o build\cargo-auditable-windows-amd64.exe
-..\..\upx-4.2.1-win64\upx.exe -9 --lzma build\cargo-auditable-windows-amd64.exe
+..\..\upx-4.2.2-win64\upx.exe -9 --lzma build\cargo-auditable-windows-amd64.exe
 copy build\* ..\..\plugins\cargo-auditable\
 Remove-Item build -Recurse -Force
 cd ..\..
@@ -35,12 +35,12 @@ cd ..\..
 New-Item -ItemType Directory -Path plugins\trivy -Force
 cd thirdparty\trivy
 go build -ldflags "-H=windowsgui -s -w" -o build\trivy-windows-amd64.exe
-..\..\upx-4.2.1-win64\upx.exe -9 --lzma build\trivy-windows-amd64.exe
+..\..\upx-4.2.2-win64\upx.exe -9 --lzma build\trivy-windows-amd64.exe
 copy build\* ..\..\plugins\trivy\
 Remove-Item build -Recurse -Force
 cd ..\..
 
-Remove-Item osquery-5.10.2.windows_x86_64 -Recurse -Force
-Remove-Item osquery-5.10.2.windows_x86_64.zip -Recurse -Force
-Remove-Item upx-4.2.1-win64 -Recurse -Force
-Remove-Item upx-4.2.1-win64.zip -Recurse -Force
+Remove-Item osquery-5.11.0.windows_x86_64 -Recurse -Force
+Remove-Item osquery-5.11.0.windows_x86_64.zip -Recurse -Force
+Remove-Item upx-4.2.2-win64 -Recurse -Force
+Remove-Item upx-4.2.2-win64.zip -Recurse -Force
--- build.sh
+++ build.sh
@@ -7,13 +7,13 @@ rm -rf plugins/osquery
 rm -rf plugins/dosai
 mkdir -p plugins/osquery plugins/dosai
 
-wget https://github.com/osquery/osquery/releases/download/5.10.2/osquery-5.10.2_1.linux_x86_64.tar.gz
-tar -xvf osquery-5.10.2_1.linux_x86_64.tar.gz
+wget https://github.com/osquery/osquery/releases/download/5.11.0/osquery-5.11.0_1.linux_x86_64.tar.gz
+tar -xvf osquery-5.11.0_1.linux_x86_64.tar.gz
 cp opt/osquery/bin/osqueryd plugins/osquery/osqueryi-linux-amd64
 upx -9 --lzma plugins/osquery/osqueryi-linux-amd64
 sha256sum plugins/osquery/osqueryi-linux-amd64 > plugins/osquery/osqueryi-linux-amd64.sha256
 rm -rf etc usr var opt
-rm osquery-5.10.2_1.linux_x86_64.tar.gz
+rm osquery-5.11.0_1.linux_x86_64.tar.gz
 
 curl -L https://github.com/owasp-dep-scan/dosai/releases/latest/download/Dosai -o plugins/dosai/dosai-linux-amd64
 chmod +x plugins/dosai/dosai-linux-amd64
@@ -36,7 +36,7 @@ done
 ./plugins/cargo-auditable/cargo-auditable-cdxgen-linux-amd64
 ./plugins/dosai/dosai-linux-amd64 --help
 
-for flavours in windows-amd64 arm64 ppc64
+for flavours in windows-amd64 linux-arm64 windows-arm64 darwin-arm64 ppc64
 do
     chmod +x packages/$flavours/build-$flavours.sh
     pushd packages/$flavours
