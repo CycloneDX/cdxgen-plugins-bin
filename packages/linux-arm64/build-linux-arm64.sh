6ab2ed9197812cf694ee6010b099b53caea17ff0
Tim Messing <141575989+timmyteo@users.noreply.github.com>
2024-07-10 07:31:20 -0600

    Updating all 3 plugins Signed-off-by: Tim Messing <141575989+timmyteo@users.noreply.github.com>
    Signed-off-by: Tim Messing <141575989+timmyteo@users.noreply.github.com>

--- packages/linux-arm64/build-linux-arm64.sh
+++ packages/linux-arm64/build-linux-arm64.sh
@@ -5,13 +5,13 @@ rm -rf plugins/osquery
 rm -rf plugins/dosai
 mkdir -p plugins/osquery plugins/dosai
 
-wget https://github.com/osquery/osquery/releases/download/5.11.0/osquery-5.11.0_1.linux_aarch64.tar.gz
-tar -xvf osquery-5.11.0_1.linux_aarch64.tar.gz
+wget https://github.com/osquery/osquery/releases/download/5.12.2/osquery-5.12.2_1.linux_aarch64.tar.gz
+tar -xvf osquery-5.12.2_1.linux_aarch64.tar.gz
 cp opt/osquery/bin/osqueryd plugins/osquery/osqueryi-linux-arm64
 upx -9 --lzma plugins/osquery/osqueryi-linux-arm64
 sha256sum plugins/osquery/osqueryi-linux-arm64 > plugins/osquery/osqueryi-linux-arm64.sha256
 rm -rf etc usr var opt
-rm osquery-5.11.0_1.linux_aarch64.tar.gz
+rm osquery-5.12.2_1.linux_aarch64.tar.gz
 
 curl -L https://github.com/owasp-dep-scan/dosai/releases/latest/download/Dosai-linux-arm64 -o plugins/dosai/dosai-linux-arm64
 chmod +x plugins/dosai/dosai-linux-arm64
