651b57602bfe90b7f291d8540e9a74ff542168ae
Tim Messing <141575989+timmyteo@users.noreply.github.com>
2024-07-11 01:23:58 -0600

    Updating all 3 plugins (#20)
    * Updating all 3 plugins
    Signed-off-by: Tim Messing <141575989+timmyteo@users.noreply.github.com>
    
    Signed-off-by: Tim Messing <141575989+timmyteo@users.noreply.github.com>
    
    * Fix for freeing disk space in CI/CD
    
    Signed-off-by: Tim Messing <141575989+timmyteo@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Tim Messing <141575989+timmyteo@users.noreply.github.com>

--- build.ps1
+++ build.ps1
@@ -1,13 +1,13 @@
 New-Item -ItemType Directory -Path plugins\osquery -Force
 New-Item -ItemType Directory -Path plugins\dosai -Force
 
-Invoke-WebRequest -Uri https://github.com/upx/upx/releases/download/v4.2.2/upx-4.2.2-win64.zip -UseBasicParsing -OutFile upx-4.2.2-win64.zip
-Expand-Archive -Path upx-4.2.2-win64.zip -DestinationPath . -Force
+Invoke-WebRequest -Uri https://github.com/upx/upx/releases/download/v4.2.4/upx-4.2.4-win64.zip -UseBasicParsing -OutFile upx-4.2.4-win64.zip
+Expand-Archive -Path upx-4.2.4-win64.zip -DestinationPath . -Force
 
-Invoke-WebRequest -Uri https://github.com/osquery/osquery/releases/download/5.11.0/osquery-5.11.0.windows_x86_64.zip -UseBasicParsing -OutFile osquery-5.11.0.windows_x86_64.zip
-Expand-Archive -Path osquery-5.11.0.windows_x86_64.zip -DestinationPath . -Force
-copy "osquery-5.11.0.windows_x86_64\Program Files\osquery\osqueryi.exe" plugins\osquery\osqueryi-windows-amd64.exe
-upx-4.2.2-win64\upx.exe -9 --lzma plugins\osquery\osqueryi-windows-amd64.exe
+Invoke-WebRequest -Uri https://github.com/osquery/osquery/releases/download/5.12.2/osquery-5.12.2.windows_x86_64.zip -UseBasicParsing -OutFile osquery-5.12.2.windows_x86_64.zip
+Expand-Archive -Path osquery-5.12.2.windows_x86_64.zip -DestinationPath . -Force
+copy "osquery-5.12.2.windows_x86_64\Program Files\osquery\osqueryi.exe" plugins\osquery\osqueryi-windows-amd64.exe
+upx-4.2.4-win64\upx.exe -9 --lzma plugins\osquery\osqueryi-windows-amd64.exe
 plugins\osquery\osqueryi-windows-amd64.exe --help
 
 Invoke-WebRequest -Uri https://github.com/owasp-dep-scan/dosai/releases/latest/download/Dosai.exe -UseBasicParsing -OutFile plugins/dosai/dosai-windows-amd64.exe
@@ -19,12 +19,12 @@ set GOARCH=amd64
 New-Item -ItemType Directory -Path plugins\trivy -Force
 cd thirdparty\trivy
 go build -ldflags "-H=windowsgui -s -w" -o build\trivy-windows-amd64.exe
-..\..\upx-4.2.2-win64\upx.exe -9 --lzma build\trivy-windows-amd64.exe
+..\..\upx-4.2.4-win64\upx.exe -9 --lzma build\trivy-windows-amd64.exe
 copy build\* ..\..\plugins\trivy\
 Remove-Item build -Recurse -Force
 cd ..\..
 
-Remove-Item osquery-5.11.0.windows_x86_64 -Recurse -Force
-Remove-Item osquery-5.11.0.windows_x86_64.zip -Recurse -Force
-Remove-Item upx-4.2.2-win64 -Recurse -Force
-Remove-Item upx-4.2.2-win64.zip -Recurse -Force
+Remove-Item osquery-5.12.2.windows_x86_64 -Recurse -Force
+Remove-Item osquery-5.12.2.windows_x86_64.zip -Recurse -Force
+Remove-Item upx-4.2.4-win64 -Recurse -Force
+Remove-Item upx-4.2.4-win64.zip -Recurse -Force
--- packages/windows-arm64/build-windows-arm64.sh
+++ packages/windows-arm64/build-windows-arm64.sh
@@ -5,12 +5,12 @@ rm -rf plugins/osquery
 rm -rf plugins/dosai
 mkdir -p plugins/osquery plugins/dosai
 
-wget https://github.com/osquery/osquery/releases/download/5.11.0/osquery-5.11.0.windows_arm64.zip
-unzip osquery-5.11.0.windows_arm64.zip
-cp "osquery-5.11.0.windows_arm64/Program Files/osquery/osqueryi.exe" plugins/osquery/osqueryi-windows-arm64.exe
+wget https://github.com/osquery/osquery/releases/download/5.12.2/osquery-5.12.2.windows_arm64.zip
+unzip osquery-5.12.2.windows_arm64.zip
+cp "osquery-5.12.2.windows_arm64/Program Files/osquery/osqueryi.exe" plugins/osquery/osqueryi-windows-arm64.exe
 sha256sum plugins/osquery/osqueryi-windows-arm64.exe > plugins/osquery/osqueryi-windows-arm64.exe.sha256
-rm -rf osquery-5.11.0.windows_arm64
-rm osquery-5.11.0.windows_arm64.zip
+rm -rf osquery-5.12.2.windows_arm64
+rm osquery-5.12.2.windows_arm64.zip
 
 curl -L https://github.com/owasp-dep-scan/dosai/releases/latest/download/Dosai-windows-arm64.exe -o plugins/dosai/dosai-windows-arm64.exe
 sha256sum plugins/dosai/dosai-windows-arm64.exe > plugins/dosai/dosai-windows-arm64.exe.sha256
