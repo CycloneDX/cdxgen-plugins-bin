8056a895f3e4ab8b6287705fb166c50f981c4247
prabhu <7842+prabhu@users.noreply.github.com>
2025-09-16 22:53:31 +0100

    Update dosai and osquery (#34)
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- packages/linux-arm64/build-linux-arm64.sh
+++ packages/linux-arm64/build-linux-arm64.sh
@@ -12,13 +12,13 @@ oras pull ghcr.io/cyclonedx/cdxgen-plugins-bin:linux-arm64 -o plugins/sourcekitt
 rm -f plugins/sourcekitten/trivy-cdxgen-*
 ls -l plugins/sourcekitten/
 
-wget https://github.com/osquery/osquery/releases/download/5.17.0/osquery-5.17.0_1.linux_aarch64.tar.gz
-tar -xf osquery-5.17.0_1.linux_aarch64.tar.gz
+wget https://github.com/osquery/osquery/releases/download/5.19.0/osquery-5.19.0_1.linux_aarch64.tar.gz
+tar -xf osquery-5.19.0_1.linux_aarch64.tar.gz
 cp opt/osquery/bin/osqueryd plugins/osquery/osqueryi-linux-arm64
 upx -9 --lzma plugins/osquery/osqueryi-linux-arm64
 sha256sum plugins/osquery/osqueryi-linux-arm64 > plugins/osquery/osqueryi-linux-arm64.sha256
 rm -rf etc usr var opt
-rm osquery-5.17.0_1.linux_aarch64.tar.gz
+rm osquery-5.19.0_1.linux_aarch64.tar.gz
 
 curl -L https://github.com/owasp-dep-scan/dosai/releases/latest/download/Dosai-linux-arm64 -o plugins/dosai/dosai-linux-arm64
 chmod +x plugins/dosai/dosai-linux-arm64
