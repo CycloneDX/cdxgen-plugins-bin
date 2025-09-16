6904dfd9471ccce818d572f23c158f6fd6a4521b
Prabhu Subramanian <prabhu@appthreat.com>
2025-09-16 21:09:29 +0100

    Update dosai and osquery
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- build.ps1
+++ build.ps1
@@ -2,25 +2,25 @@ New-Item -ItemType Directory -Path plugins\osquery -Force
 New-Item -ItemType Directory -Path plugins\dosai -Force
 New-Item -ItemType Directory -Path plugins\trivy -Force
 
-Invoke-WebRequest -Uri https://github.com/upx/upx/releases/download/v5.0.1/upx-5.0.1-win64.zip -UseBasicParsing -OutFile upx-5.0.1-win64.zip
-Expand-Archive -Path upx-5.0.1-win64.zip -DestinationPath . -Force
+Invoke-WebRequest -Uri https://github.com/upx/upx/releases/download/v5.0.2/upx-5.0.2-win64.zip -UseBasicParsing -OutFile upx-5.0.2-win64.zip
+Expand-Archive -Path upx-5.0.2-win64.zip -DestinationPath . -Force
 
-Invoke-WebRequest -Uri https://github.com/osquery/osquery/releases/download/5.17.0/osquery-5.17.0.windows_x86_64.zip -UseBasicParsing -OutFile osquery-5.17.0.windows_x86_64.zip
-Expand-Archive -Path osquery-5.17.0.windows_x86_64.zip -DestinationPath . -Force
-copy "osquery-5.17.0.windows_x86_64\Program Files\osquery\osqueryi.exe" plugins\osquery\osqueryi-windows-amd64.exe
-upx-5.0.1-win64\upx.exe -9 --lzma plugins\osquery\osqueryi-windows-amd64.exe
+Invoke-WebRequest -Uri https://github.com/osquery/osquery/releases/download/5.19.0/osquery-5.19.0.windows_x86_64.zip -UseBasicParsing -OutFile osquery-5.19.0.windows_x86_64.zip
+Expand-Archive -Path osquery-5.19.0.windows_x86_64.zip -DestinationPath . -Force
+copy "osquery-5.19.0.windows_x86_64\Program Files\osquery\osqueryi.exe" plugins\osquery\osqueryi-windows-amd64.exe
+upx-5.0.2-win64\upx.exe -9 --lzma plugins\osquery\osqueryi-windows-amd64.exe
 plugins\osquery\osqueryi-windows-amd64.exe --help
 
 Invoke-WebRequest -Uri https://github.com/owasp-dep-scan/dosai/releases/latest/download/Dosai.exe -UseBasicParsing -OutFile plugins/dosai/dosai-windows-amd64.exe
 
 cd thirdparty\trivy
 go build -ldflags "-H=windowsgui -s -w" -o build\trivy-windows-amd64.exe
-..\..\upx-5.0.1-win64\upx.exe -9 --lzma build\trivy-windows-amd64.exe
+..\..\upx-5.0.2-win64\upx.exe -9 --lzma build\trivy-windows-amd64.exe
 copy build\* ..\..\plugins\trivy\
 Remove-Item build -Recurse -Force
 cd ..\..
 
-Remove-Item osquery-5.17.0.windows_x86_64 -Recurse -Force
-Remove-Item osquery-5.17.0.windows_x86_64.zip -Recurse -Force
-Remove-Item upx-5.0.1-win64 -Recurse -Force
-Remove-Item upx-5.0.1-win64.zip -Recurse -Force
+Remove-Item osquery-5.19.0.windows_x86_64 -Recurse -Force
+Remove-Item osquery-5.19.0.windows_x86_64.zip -Recurse -Force
+Remove-Item upx-5.0.2-win64 -Recurse -Force
+Remove-Item upx-5.0.2-win64.zip -Recurse -Force
--- packages/linux-amd64/build-linux-amd64.sh
+++ packages/linux-amd64/build-linux-amd64.sh
@@ -10,14 +10,14 @@ sha256sum plugins/sourcekitten/sourcekitten > plugins/sourcekitten/sourcekitten.
 rm -f plugins/sourcekitten/trivy-cdxgen-*
 ls -l plugins/sourcekitten/
 
-wget https://github.com/osquery/osquery/releases/download/5.17.0/osquery-5.17.0_1.linux_x86_64.tar.gz
-tar -xf osquery-5.17.0_1.linux_x86_64.tar.gz
+wget https://github.com/osquery/osquery/releases/download/5.19.0/osquery-5.19.0_1.linux_x86_64.tar.gz
+tar -xf osquery-5.19.0_1.linux_x86_64.tar.gz
 cp opt/osquery/bin/osqueryd plugins/osquery/osqueryi-linux-amd64
 upx -9 --lzma plugins/osquery/osqueryi-linux-amd64
 ./plugins/osquery/osqueryi-linux-amd64 --help
 sha256sum plugins/osquery/osqueryi-linux-amd64 > plugins/osquery/osqueryi-linux-amd64.sha256
 rm -rf etc usr var opt
-rm osquery-5.17.0_1.linux_x86_64.tar.gz
+rm osquery-5.19.0_1.linux_x86_64.tar.gz
 
 # Download the Dosai binary
 curl -L https://github.com/owasp-dep-scan/dosai/releases/latest/download/Dosai-linux-amd64 -o plugins/dosai/dosai-linux-amd64
